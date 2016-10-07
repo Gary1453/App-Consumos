cf19505c4f6265a22c3002ea7444eca07b748794
Gary1453 <bdahfbj@gmail.com>
2016-10-07 13:55:50 -0500

    Version Inicial de la aplicación

---
+++ bienvenido.php
@@ -0,0 +1,65 @@
+
+<?php
+
+session_start();
+//$_SESSION['nombre']="gary";
+//$nombre=$_SESSION['nombre'];
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+        
+	<meta charset=”utf8”>
+	<title> Login Bank </title>
+	
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+</head>
+
+
+<body>
+
+
+        <header>
+
+
+        </header>
+
+		<h1 align="center">
+
+			<?php
+
+			$nombre=$_SESSION['nombre'];
+			 echo 'Bienvenido'. ' ' . $nombre;
+
+			?>
+
+		</h1>
+
+		<div id="piechart" style="width: 900px; height: 500px;" ></div>
+
+
+
+
+
+
+        <footer>
+
+        
+        </footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>   
+<script type="text/javascript" src="js/bienvenido.js"></script>
+
+
+</body>
+
+
+</html>
---
+++ js/bienvenido.js
@@ -0,0 +1,102 @@
+
+		
+		google.charts.load('current', {'packages':['corechart']});
+		//google.charts.setOnLoadCallback( drawChart );
+		google.charts.setOnLoadCallback( function(){
+
+				
+
+			var arreglo = cargarArreglo();
+			console.log(arreglo);
+			
+			setTimeout(function(){
+
+				drawChart(arreglo);
+
+			},15000);
+
+			
+
+		});
+
+
+		function drawChart(arreglo) {
+
+		/*var arreglo = [	['Familia', 'ImporteSoles'],['OTROS', 10],	
+		['Eat',2],['Commute',2],['Watch TV', 2],['Sleep',    7] ];*/
+		//var arreglo = cargarArreglo(arreglo);
+		//console.log(arreglo);
+
+
+		var data = google.visualization.arrayToDataTable(arreglo);
+
+		var options = {
+		title: 'Mis Consumos Diarios'
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+		function selectHandler() {
+
+		var selectedItem = chart.getSelection()[0];
+		
+			if (selectedItem) {
+			var familia = data.getValue(selectedItem.row, 0);
+			//alert('The user selected ' + topping);
+			window.location.href="http://localhost/App Consumos/gastos_mensuales.html?familia="+familia;
+	
+			}
+
+		}
+
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+
+
+		chart.draw(data, options);
+
+		}
+
+
+	// El siguiente metodo nos permite obtener un arreglo de los consumos de un cliente dado
+
+		function cargarArreglo()
+
+		{
+
+	        var url="http://localhost/App Consumos/logica.php";
+	        url = url + "?opcion=1&callback=?";       
+
+	        var arreglo = [  
+
+				['Familia', 'ImporteSoles'],
+				['OTROS',0], ['ENTRETENIMIENTO',0], ['TIENDA POR DEPARTAMENTOS',0], 
+				['RESTAURANTES',0], ['PAGOS VARIOS',0],['EDUCACION',0],
+				['ESTACIONES DE SERVICIO',0],['SALUD',0],['SERVICIOS ESPECIALIZADOS',0],
+				['SUPERMERCADOS',0] ,['TRANSPORTE',0] 
+				  		  ];	
+
+	        
+	        var prueba = $.getJSON( url, function(presultado)
+	        {                
+
+				for(i=0 ; i< presultado.length ; i++)
+				{
+		 		
+				 		arreglo.find(function(variable)
+				 		{
+
+					 			if( variable[0] == presultado[i].FAMILIA )
+					 			{
+
+					 				variable[1] = parseInt( presultado[i].IMPORTE );
+					 			}
+
+				 		});
+
+				}			
+
+	        });
+					  
+		return arreglo;				  
+
+    	}
